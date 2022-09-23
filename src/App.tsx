6cb166e58f326ea5c995fd2c530f4d000c9cb799
Michal Berg <michalb@stud.ntnu.no>
2022-09-23 13:49:11 +0200

    Added GlobalStyles, and implemented it in the App.tsx file. Created a linear gradient as background and added it to the GlobalStyles file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Home from "./Containers/HomePage/Home";
 import Contact from "./Containers/ContactPage/Contact";
 import About from "./Containers/AboutPage/About";
@@ -8,36 +8,37 @@ import Termsofservice from './Containers/TermsOfService/Termsofservice';
 import Privacypolicy from './Containers/PrivacyPolicy/Privacypolicy';
 import Faq from './Containers/FAQ/Faq';
 import Usermanual from './Containers/UserManual/Usermanual';
-
+import GlobalStyle from "./Styles/Global/GlobalStyles";
 
 
 function App() {
 
-  return (
-    <div className={"Talmoe-app"}>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path={"/contact"}>
-        <Contact />
-      </Route>
-      <Route exact path={"/about"}>
-        <About />
-      </Route>
-      <Route exact path={"/termsofservice"}>
-        <Termsofservice />
-      </Route>
-      <Route exact path={"/privacypolicy"}>
-        <Privacypolicy />
-      </Route>
-      <Route exact path={"/faq"}>
-        <Faq />
-      </Route>
-      <Route exact path={"/usermanual"}>
-        <Usermanual />
-      </Route>
-    </div>
-  );
+    return (
+        <div className={"Talmoe-app"}>
+            <Route exact path="/">
+                <GlobalStyle/>
+                <Home/>
+            </Route>
+            <Route exact path={"/contact"}>
+                <Contact/>
+            </Route>
+            <Route exact path={"/about"}>
+                <About/>
+            </Route>
+            <Route exact path={"/termsofservice"}>
+                <Termsofservice/>
+            </Route>
+            <Route exact path={"/privacypolicy"}>
+                <Privacypolicy/>
+            </Route>
+            <Route exact path={"/faq"}>
+                <Faq/>
+            </Route>
+            <Route exact path={"/usermanual"}>
+                <Usermanual/>
+            </Route>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Styles/Global/GlobalStyles.tsx
@@ -0,0 +1,19 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  //html {
+  //  margin: 2rem;
+  //}
+
+  // removes default styling from link tags
+  a:-webkit-any-link {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  body {
+    background: linear-gradient(180deg, #28283A -2.04%, rgba(32, 27, 33, 0.9) 102.12%, rgba(40, 40, 58, 0) 102.13%);
+  }
+`;
+
+export default GlobalStyle;
