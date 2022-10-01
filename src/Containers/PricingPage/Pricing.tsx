f817cd8adf474d137d114fe84c287a1bc0a13423
Michal Berg <michalb@stud.ntnu.no>
2022-10-01 21:42:23 +0200

    Added initial layout for pricing page.

--- src/Containers/PricingPage/Pricing.tsx
+++ src/Containers/PricingPage/Pricing.tsx
@@ -6,9 +6,19 @@ import Footer from '../../Components/Footer/Footer';
 import {OuterContainer} from "../../Components/MainLayout/MainLayout";
 
 
+const MainSection = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+`;
+
 function Pricing() {
     return <OuterContainer>
+        <Header/>
+        <MainSection>
 
+        </MainSection>
+        <Footer/>
     </OuterContainer>
 }
 
