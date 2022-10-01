7612adcfed2a5513baf265239136bad4086419b0
MicBerg <michal.berg99@gmail.com>
2022-10-01 14:36:43 +0200

    Fixed GlobalStyling for all containers.

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ function App() {
 
     return (
         <div className={"Talmoe-app"}>
+            <GlobalStyle/>
             <Route exact path="/">
-                <GlobalStyle/>
                 <Home/>
             </Route>
             <Route exact path={"/contact"}>
